db2980c956cce14c5a0ae0b9261b0dfe97977ca1
Kian van der Meer <kiansinan@gmail.com>
2023-12-20 12:55:39 +0100

    only dockerize frontend

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install necessary dependencies
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-venv nodejs npm
+
+# Set up and run the Flask server
+cd backend
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+echo "FLASK_SECRET_KEY: kkkkjfjksdjkljkfslkjqjkjklklkas
+DASHBOARD_UPDATE_TIME: 5
+DEV: false" > config.yml
+
+python3 app.py &
+
+# Set up and run the Vue frontend (in Docker)
+cd ../frontend
+docker-compose up -d
