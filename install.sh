6bfa1a3391437ad556cdb135a64abbc3cb949045
Kian van der Meer <kiansinan@gmail.com>
2023-12-20 14:13:16 +0100

    switched order of back and front

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 sudo apt-get update
 sudo apt-get install -y python3-pip python3-venv
 
+# Set up and run the Vue frontend (in Docker)
+cd ../frontend
+docker-compose up -d
+
 # Set up and run the Flask server
 cd backend
 python3 -m venv venv
@@ -14,8 +18,4 @@ echo "FLASK_SECRET_KEY: kkkkjfjksdjkljkfslkjqjkjklklkas
 DASHBOARD_UPDATE_TIME: 5
 DEV: false" > config.yml
 
-python3 app.py &
-
-# Set up and run the Vue frontend (in Docker)
-cd ../frontend
-docker-compose up -d
+python3 app.py
